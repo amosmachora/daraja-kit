869b8d517c6a1332f87fa35b6e3d5950cb4dffaa
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2023-12-26 15:08:39 +0300

    fixed tsup erors

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["index.ts"],
+  entry: ["./src/index.ts"],
   dts: true,
   format: ["esm", "cjs"],
   outDir: "dist", // Specify the output directory for your files
