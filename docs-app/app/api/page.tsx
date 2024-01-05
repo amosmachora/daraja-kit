ee54a094c9a94b26ebe3edda15119a835ef9a4ed
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2024-01-05 18:36:05 +0300

    fixed sidebar link error redirecting to localhost

--- docs-app/app/api/page.tsx
+++ docs-app/app/api/page.tsx
@@ -58,7 +58,7 @@ const Page = () => {
         <p className="text-myPurple font-semibold">On this page</p>
         {links.map((l) => (
           <a
-            href={`http://localhost:3000/api#${l}`}
+            href={`https://react-daraja.vercel.app/api#${l}`}
             className="bg-gray-100 text-sm font-semibold px-2 hover:text-myPurple ml-2 overflow-clip"
             key={l}
           >
