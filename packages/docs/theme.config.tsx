92d3a606076c15d8eb0fb9e569ca1f7249c626d2
Amos Machora <amosmachora13@gmail.com>
2024-11-04 10:44:27 +0300

    fix: use the correct page title

--- packages/docs/theme.config.tsx
+++ packages/docs/theme.config.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
   logo: <span>Daraja Kit</span>,
@@ -8,7 +9,27 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: "https://github.com/amosmachora/daraja-kit",
   footer: {
-    content: "Nextra Docs Template",
+    content: "Daraja kit docs",
+  },
+  head: () => {
+    const { title, frontMatter } = useConfig();
+    const { asPath, defaultLocale, locale } = useRouter();
+    const url =
+      "https://daraja-kit.amosmachora.com" +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+    return (
+      <>
+        <title>
+          {title ? `${title} | Daraja Kit` : "Daraja Kit Documentation"}
+        </title>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || "Daraja Kit"} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || "Daraja Kit Documentation"}
+        />
+      </>
+    );
   },
 };
 
